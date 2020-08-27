1ef1c645159f8048be73009beb1f3ebda5774504
daleinen7 <daleinen@gmail.com>
2020-08-27 15:21:03 -0500

    be in the midst of connecting a remote database and have an untested OAuth implementation

---
+++ models/user.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: String,
+  email: String,
+  googleId: String,
+  followedSets: [{type: mongoose.Schema.Types.ObjectId, ref: "Set"}]
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('User', userSchema);
---
+++ routes/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Homepage for The Collabratory' });
+});
+
+// Google OAuth login route
+router.get('/auth/google', passport.authenticate(
+  'google',
+  { scope: ['profile', 'email'] }
+));
+
+// Google OAuth callback route
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+  successRedirect : '/',
+  failureRedirect : '/'
+  }
+));
+
+// OAuth logout
+router.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/users');
+});
+
+module.exports = router;
