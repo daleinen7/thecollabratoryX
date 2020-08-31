434f1f997935f1417a52b30ca0ff5e27e3f65090
daleinen7 <daleinen@gmail.com>
2020-08-30 23:07:15 -0500

    pull followed sets into individual arrays

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,36 @@ router.get('/', function(req, res, next) {
   // If the user is logged in show their followed sets
   if (req.user) {
     Set.find({'followedBy': {$in: req.user._id}}, function(err, followedSets) {
+      const instrument = [];
+      const effect = [];
+      const role = [];
+      const universal = [];
+      
+      for (let i = 0; i < followedSets.length; i++) {
+        for (let j = 0; j < followedSets[i].entries.length; j++) {
+          ent = followedSets[i].entries[j];
+          
+          switch (ent.category) {
+            case 'instrument':
+              instrument.push(ent.description);
+              break;
+            case 'effect':
+              effect.push(ent.description);
+              break;
+            case 'role':
+              role.push(ent.description);
+              break;
+            case 'universal': 
+              universal.push(ent.description);
+              break;
+            default:
+              break;
+          }
+        }
+      }
+      
+      console.log(instrument);
+
       Set.find({'followedBy': {$nin: req.user._id}}, function(err, unfollowedSets) {
         res.render('index', { 
           title: 'Homepage for The Collabratory',
